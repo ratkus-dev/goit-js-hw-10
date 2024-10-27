ecae1ffcc48d0c11ec028fe6d4d908fceb9cd141
vitalysementsov <v.sementsov@ratkus.com.ua>
2024-10-28 00:51:57 +0200

    fixed icons import

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig(({ command }) => {
     build: {
       sourcemap: true,
       rollupOptions: {
+        external: [/\.svg$/],
         input: glob.sync('./src/*.html'),
         output: {
           manualChunks(id) {
